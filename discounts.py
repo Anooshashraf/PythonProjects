5ca9a4bee0a4980301cc45b2135c688605924992
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-03-23 17:53:00 +0500

    ..

--- discounts.py
+++ discounts.py
@@ -3,6 +3,8 @@ import tkinter as tk
 
 class discounts():
     def __init__(self , root):
+
+        
         self.root = root #to initialize the root
         self.root.title("Discount Sales")
 
@@ -10,7 +12,9 @@ class discounts():
         self.width = self.root.winfo_screenwidth() #here winfo gives information about the entire window
         self.height = self.root.winfo_screenheight()  
         self.root.geometry(f"{self.width}x{self.height}+0+0") #here +0 and +0 refers to that we want our window to start from firt row and first column
-        
+        title = tk.Label(self.root, text="Discount Sales Management" , bd=3 , relief="groove" , font=("Times New Roman",50, "bold"), bg="gray")
+        title.pack(side="top" , fill="x")
+
 
 root = tk.Tk()
 obj = discounts(root)
