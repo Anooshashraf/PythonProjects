bc3f5f9cdf7ce150aef62171a5e61651966bdf72
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-03-23 17:28:52 +0500

    initializing the sql-python project

---
+++ discounts.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+
+class discounts():
+    def __init__(self , root):
+        self.root = root #to initialize the root
+        self.root.title("Discount Sales")
+
+        # now we need to define the global varivable like self.variableName
+        self.width = self.root.winfo_screenwidth() #here winfo gives information about the entire window
+        self.height = self.root.winfo_screenheight()  
+        self.root.geometry(f"{self.width}x{self.height}+0+0") #here +0 and +0 refers to that we want our window to start from firt row and first column
+        
+
+root = tk.Tk()
+obj = discounts(root)
+root.mainloop()  # main loop is used to handle all the events in our projects
