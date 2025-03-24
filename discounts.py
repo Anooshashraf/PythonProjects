01065c616afd8220fc822e5afbdbf524471ea12a
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-03-24 23:46:26 +0500

    ..

--- discounts.py
+++ discounts.py
@@ -1,4 +1,10 @@
 import tkinter as tk
+from tkinter import ttk
+import pymysql  
+from tkinter import messagebox
+import datetime
+
+
 
 
 class discounts():
@@ -18,16 +24,64 @@ class discounts():
         saleFrame = tk.Frame(self.root, bd=4 , relief="ridge" , bg=self.clr(200,180,180))
         saleFrame.place(width=self.width/3 , height=self.height-180 , x=30 ,y=100)
 
-        self.detailFrame = tk.Frame(self.root, bd=4 ,relief="raised" , bg=self.clr(180,180,200) )
+        self.detailFrame = tk.Frame(self.root, bd=4 ,relief="raised" , bg=self.clr(180,180,200))
         self.detailFrame.place(width=self.width * 1/2 + 50 , height = self.height - 180 , x = self.width/3 + 100 , y=100)
+        head = tk.Label(self.detailFrame, text="Product Details",bg=self.clr(180,180,200), font=("Times New Roman",40, "bold"))
+        head.pack(side="top" , fill="x")
         self.table()
 
     def clr(self,r,g,b):
         return f"#{r:02x}{g:02x}{b:02x}"
     
     def table(self):
-        tableFrame = tk.Frame(self.detailFrame , bd=4 , relief="raised" , bg="purple")
-        tableFrame.place(width=self.width/2 + 10 , height=self.height - 280, x=20 , y=80)
+        tableFrame = tk.Frame(self.detailFrame , bd=4 , relief="raised" , bg="cyan")
+        tableFrame.place(width=self.width/2 + 10 , height=self.height - 280, x=15 , y=80)
+
+
+        
+        x_scroll = tk.Scrollbar(tableFrame,orient="horizontal")
+        x_scroll.pack(side="bottom",fill="x")
+
+        y_scroll = tk.Scrollbar(tableFrame,orient="vertical")
+        y_scroll.pack(side="right",fill="y")
+
+        self.table = ttk.Treeview(tableFrame , columns=("item","price","quantity","Expire"), xscrollcommand = x_scroll.set , yscrollcommand = y_scroll.set)
+        x_scroll.config(command=self.table.xview)
+        y_scroll.config(command=self.table.yview)
+
+        self.table.heading("item",text="ITEMS")
+        self.table.heading("price",text="PRICE")
+        self.table.heading("quantity",text="QUANTITY")
+        self.table.heading("Expire",text="EXPIRY")
+        self.table["show"]="headings"
+
+        self.table.pack(fill="both" , expand=1)
+
+    def disc(self):
+        try:
+            self.dbFunc()
+            self.cursor.execute("select * from sales")
+            data = self.cur.fetchall()
+            today = datetime.now().date()
+
+            for i,pr,quan,exp in data:
+                if quan > 0 and exp==today:
+                    pass
+
+
+
+
+
+        except Exception as e:
+            tk.messagebox.showerror("Error" , f"Error: {e}")
+
+
+    def dbFunc(self):
+        self.con = pymysql.Connect(host="localhost", user="root", passwd="localhost123" ,database="discounts")
+        self.cursor = self.con.cursor()
+
+        
+
 
 root = tk.Tk() 
 obj = discounts(root)
