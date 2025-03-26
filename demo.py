40e5b1ca82746ab1d03df712753def33a045be51
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-03-26 21:24:09 +0500

    python and sql #02

--- demo.py
+++ demo.py
@@ -8,9 +8,19 @@ mydb = pymysql.connect(
 )
 
 mycur = mydb.cursor() #initialinzing the cursor that is going to interact with our database
+
+sqlFormula = " INSERT INTO STUDENTS (Name,FullName,Age) VALUES (%s,%s,%s) "# these %s can be molded to any form of data afterwards
+studentArr = [("Varsha"," Varsha Mobeen",19),
+              ("Maaz"," Maaz Anwar",20),
+              ("Ans"," Ans Ashraf",23),
+              ("Mair"," Mair Ashraf",13)]
+
+
+mycur.executemany(sqlFormula ,studentArr)
+
+
+mydb.commit()
+
 # mycur.execute(" CREATE DATABASE testdb ")
 # mycur.execute(" SHOW DATABASES ")
 # mycur.execute(" CREATE TABLE STUDENTS(Name varchar(25) ,FullName varchar(25), Age int(10)  )")
-mycur.execute(" SHOW TABLES ")
-for i in mycur:
-    print(i)
